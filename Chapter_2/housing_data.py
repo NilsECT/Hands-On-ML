ed5dd027dda2da0fb985349b3ef01363d45e4933
Nils T <n.e.c.taugbol@fys.uio.no>
2023-04-08 17:10:11 +0200

    took a look at the data

--- Chapter_2/housing_data.py
+++ Chapter_2/housing_data.py
@@ -3,6 +3,9 @@ import pandas as pd
 import tarfile
 import urllib.request
 
+# creating the function to fetch the data separately use by calling
+# from housing_data import load_housing_data
+
 def load_housing_data():
     '''
     Fetches the housing data ffor chapter 2 of HOML
@@ -28,3 +31,38 @@ def load_housing_data():
             housing_tarball.extractall(path="datasets")
 
     return pd.read_csv(Path("datasets/housing/housing.csv"))
+
+# to take a look at the data follow the following
+# using the data and such will not be done here
+# some or all of the following might be in the main project file
+
+housing = load_housing_data()
+
+# printing the first five rows using pd.head
+print("Showing the top five rows of the housing data:")
+print(housing.head())
+
+print()
+# printing info from each column
+print("Showing the info for all the columns:")
+print(housing.info())
+
+print()
+# ocean_proximity is dtype Object, take a closer look at it
+print("Taking a closer look at the ocean_proximity feature:")
+print(housing["ocean_proximity"].value_counts())
+
+print()
+# summary of the numerical attributes
+print("Showing a numerical summary of the attributes (features):")
+print(housing.describe())
+
+# plotting the data in histograms, this can be done directly from the dataframe!
+# needs matplotlib.pyplot
+
+import matplotlib.pyplot as plt
+
+housing.hist(bins=50, figsize=(14, 10))
+# plt.show()
+plt.savefig("./figs/housing_hist.pdf")
+plt.close()
